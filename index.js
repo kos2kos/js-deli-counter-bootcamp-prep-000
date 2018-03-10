26837d8c6fe4902bd4266a102ba3a07b34cfbff1
Nkosi Enciso Givhan <koasterman@gmail.com>
2018-03-10 05:09:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 function takeANumber(katzDeliLine, name){
   katzDeliLine = katzDeliLine.push(name)
-  return  `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+  var i = katzDeliLine.length
+  return  `Welcome, ${name}. You are number ${i} in line.`
 }
