bde2bc8d8b6e9b0eba6ebbb9056ee2324f02a030
Nkosi Enciso Givhan <koasterman@gmail.com>
2018-03-10 05:50:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function currentLine(line){
   if (line.length === 0){
     return "The line is currently empty."
   } if (line.length > 0) {
-    for (var i = 0; i < line; i++){
+    for (var i = 0; i < line.length; i++){
       temp = temp + ` ${i + 1}. ${line[i]},`
     }return temp
   }
